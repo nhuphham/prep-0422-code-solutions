2ee9c47644e8e42eff85abd10807adf6370854d5
“Nhu <nhu.phamx@yahoo.com>
2022-04-27 03:31:43 +0000

    completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,16 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+
+console.log('languages array', languages);
+
+var firstElement = languages[0];
+console.log('The first element of the array is', firstElement);
+
+var thirdElement = languages[2];
+console.log('the 3rd element of the array is', thirdElement);
+
+var length = languages.length;
+console.log('the Length of the array is', length);
+
+var lastIndex = length - 1;
+var lastElement = languages[lastIndex];
+console.log('the last element of the array is', lastElement);
